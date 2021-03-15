9e4fa5b358f5cf12669a7fe9bdf88bab2c8feb4e
JXAO <megaman305@hotmail.com>
2021-03-15 15:49:43 +0100

    Added RecipeActivity, connected activities created so far
    self explanatory

--- AndroidApp/app/src/main/java/com/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/LoginActivity.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.cookingmatesapp.MainActivity;
 import com.example.cookingmatesapp.R;
 
 public class LoginActivity extends AppCompatActivity {
