2ce60c52e1077eddbe5a0f72b571d4259a2d271b
ezraleeuwenhage <ezra1999@hotmail.nl>
2021-03-15 16:10:16 +0100

    Added registration activity and connected loginactivity to registeractivity through a button.

--- AndroidApp/app/src/main/java/com/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/LoginActivity.java
@@ -1,26 +1,30 @@
-//package com;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//
-//import androidx.annotation.Nullable;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.example.cookingmatesapp.MainActivity;
-//import com.example.cookingmatesapp.R;
-//
-//public class LoginActivity extends AppCompatActivity {
-//
-//    private Button login;
-//
-//    @Override
-//    protected void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
-//        login = (Button)findViewById(R.id.signInBtn);
-//        login.setOnClickListener(new View.OnClickListener() {
+package com;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cookingmatesapp.MainActivity;
+import com.example.cookingmatesapp.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button login;
+    private TextView register;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // define login button
+        login = (Button)findViewById(R.id.signInBtn);
+        login.setOnClickListener(this);
 //            @Override
 //            public void onClick(View v) {
 //                Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
@@ -29,5 +33,25 @@
 //                finish();
 //            }
 //        });
-//    }
-//}
+
+        // define register button
+        register = (TextView) findViewById(R.id.textView4);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // TODO fix this stuff
+            case R.id.signInBtn:
+                Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(myIntent);
+                //optional
+                finish();
+                break;
+            case R.id.textView4:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
+    }
+}
---
+++ AndroidApp/app/src/main/java/com/RegisterUser.java
@@ -0,0 +1,21 @@
+package com;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cookingmatesapp.MainActivity;
+import com.example.cookingmatesapp.R;
+
+public class RegisterUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
