cbf089f8da0f64359055931e07931ca7c651dd97
Dalton Harmsen <dylandalton27@gmail.com>
2021-03-21 18:07:59 +0100

    added navigation bar to home activity

--- AndroidApp/app/src/main/java/com/HomeActivity.java
+++ AndroidApp/app/src/main/java/com/HomeActivity.java
@@ -1,6 +1,10 @@
 package com;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -8,19 +12,26 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import com.example.cookingmatesapp.R;
+import com.google.android.material.navigation.NavigationView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ImageButton recipe1;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +46,20 @@ public class HomeActivity extends AppCompatActivity {
 
         ServerCallsApi api = retrofit.create(ServerCallsApi.class);
          */
+
+        // Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         recipe1 = (ImageButton)findViewById(R.id.img_btn);
         recipe1.setOnClickListener(new View.OnClickListener() {
