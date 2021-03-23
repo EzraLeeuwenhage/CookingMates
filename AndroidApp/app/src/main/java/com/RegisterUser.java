4e7b041911aa9bf7025addde50ca2a7ebfffbb2b
ezraleeuwenhage <ezra1999@hotmail.nl>
2021-03-23 14:36:09 +0100

    Added validity check for entered date in registration activity

--- AndroidApp/app/src/main/java/com/RegisterUser.java
+++ AndroidApp/app/src/main/java/com/RegisterUser.java
@@ -10,6 +10,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -20,6 +22,9 @@ import com.example.cookingmatesapp.R;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -55,29 +60,44 @@ public class RegisterUser extends AppCompatActivity {
         // retrieving entered data
         String username = editTextUsername.getText().toString();
         String name = editTextFullName.getText().toString();
-        Date date = new Date();
         String email = editTextEmail.getText().toString();
         String password = editTextPassword.getText().toString();
 
+        // create window to show response
         TextView responseView = findViewById(R.id.textViewResult);
-        // TODO add option for profile picture and work out issues adult stuff & email stuff
-        User user = new User(username, name, email, password, date, null, true);
-        Call<User> call = api.createUser(user);
-        call.enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                if(!response.isSuccessful()){
-                    responseView.setText("Code: " + response.code());
-                    return;
-                }
-                User user = response.body();
-                responseView.setText(user.getName() + " created!");
-            }
 
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                responseView.setText(t.getMessage());
+        try {
+            String dateAsString = editTextAge.getText().toString();
+            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+            format.setLenient(false);
+            Date date = format.parse(dateAsString);
+            System.out.println("date: " + date.toString());
+
+            // TODO add option for profile picture and adult stuff
+            // TODO add email verification?
+            User user = new User(username, name, email, password, date, null, true);
+            Call<User> call = api.createUser(user);
+            call.enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    if(!response.isSuccessful()){
+                        responseView.setText("Code: " + response.code());
+                        return;
+                    }
+                    User user = response.body();
+                    responseView.setText(user.getName() + " created!");
+                }
+
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+                    responseView.setText(t.getMessage());
+                }
+            });
+        } catch (Exception e) {
+            if (e.getClass() == ParseException.class) {
+                responseView.setText("The entered date of birth was not valid, please enter the " +
+                        "date in the following format: (dd-mm-yyyy)");
             }
-        });
+        }
     }
 }
