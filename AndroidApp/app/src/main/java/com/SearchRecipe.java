bd8af4c57f92e28ffb069b9fff404d052e65068d
tjheij <tjheij@gmail.com>
2021-03-30 11:49:41 +0200

    Setup for search recipe

--- AndroidApp/app/src/main/java/com/SearchRecipe.java
+++ AndroidApp/app/src/main/java/com/SearchRecipe.java
@@ -10,29 +10,25 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cookingmatesapp.R;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchRecipe extends AppCompatActivity {
 
     private ServerCallsApi api;
-    private TextView textViewTitle;
-    private TextView textViewDescription;
-    private EditText editTextId;
-    private EditText editTextTitle;
-    private ImageView imageView;
+    private EditText editSearchBar;
 
     private Bitmap bitmapImage;
-    private Recipe recipe;
+    private List<Recipe> recipes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,86 +41,52 @@ public class SearchRecipe extends AppCompatActivity {
                 .build();
         api = retrofit.create(ServerCallsApi.class);
 
-        textViewTitle = findViewById(R.id.textViewResult);
-        textViewDescription = findViewById(R.id.textViewDescription);
-        editTextId = findViewById(R.id.editTextNumber);
-        editTextTitle = findViewById(R.id.editTitle);
-        imageView = findViewById(R.id.imageView2);
+        editSearchBar = findViewById(R.id.editSearchBar);
     }
 
+    //Get methods get recipes from server database and put them in recipes list
+
+    //Gets all recipes
     public void getRecipes(View view){
         Call<List<Recipe>> call = api.getRecipes();
-        call.enqueue(new Callback<List<Recipe>>() {
-            @Override
-            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
-                if(!response.isSuccessful()){
-                    textViewTitle.setText("Code: " + response.code());
-                    return;
-                }
-                List<Recipe> posts = response.body();
-                recipe = posts.get(0);
-                getImage(imageView);
-
-                textViewTitle.setText(recipe.getName());
-                textViewDescription.setText(recipe.getDescription());
-            }
-
-            @Override
-            public void onFailure(Call<List<Recipe>> call, Throwable t) {
-                textViewTitle.setText(t.getMessage());
-            }
-        });
+        makeCall(call);
     }
 
-    public void getRecipeById(View view){
-        int id = Integer.parseInt(editTextId.getText().toString());
-        Call<List<Recipe>> call = api.getRecipe(id);
-        call.enqueue(new Callback<List<Recipe>>() {
-            @Override
-            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
-                if(!response.isSuccessful()){
-                    textViewTitle.setText("Code: " + response.code());
-                    return;
-                }
-                List<Recipe> posts = response.body();
-                Recipe first = posts.get(0);
-                textViewTitle.setText(first.getName());
-                textViewDescription.setText(first.getDescription());
-            }
-
-            @Override
-            public void onFailure(Call<List<Recipe>> call, Throwable t) {
-                textViewTitle.setText(t.getMessage());
-            }
-        });
-    }
-
+    //Gets all recipes where the search string is in the name
     public void getRecipeByTitle(View view){
-        String title = editTextTitle.getText().toString();
+        String title = editSearchBar.getText().toString();
         Call<List<Recipe>> call = api.getRecipeByTitle(title);
+        makeCall(call);
+    }
+
+    //Gets all recipes where the search string is in at least one ingredient
+    public void getRecipeByIngredient(View view){
+        String ingredient = editSearchBar.getText().toString();
+        Call<List<Recipe>> call = api.getRecipeByIngredient(ingredient);
+        makeCall(call);
+    }
+
+    //Actually makes the call for the get methods
+    public void makeCall(Call<List<Recipe>> call){
         call.enqueue(new Callback<List<Recipe>>() {
             @Override
             public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
                 if(!response.isSuccessful()){
-                    textViewTitle.setText("Code: " + response.code());
+                    Toast.makeText(getApplicationContext(), "Code: " + response.code(), Toast.LENGTH_SHORT);
                     return;
                 }
-                List<Recipe> posts = response.body();
-                if(posts.size() > 0) {
-                    Recipe first = posts.get(0);
-                    textViewTitle.setText(first.getName());
-                    textViewDescription.setText(first.getDescription());
-                }
+                recipes = response.body();
             }
 
             @Override
             public void onFailure(Call<List<Recipe>> call, Throwable t) {
-                textViewTitle.setText(t.getMessage());
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT);
             }
         });
     }
 
-    public void getImage(ImageView view){
+    //Retrieves the image of the input recipe and places it in the input view
+    public void getImage(ImageView view, Recipe recipe){
         String path = "http://134.209.92.24:3000/uploads" + recipe.getFilename();
         Picasso.get().load(path).into(new Target() {
 
@@ -133,15 +95,7 @@ public class SearchRecipe extends AppCompatActivity {
 
             @Override
             public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom arg1) {
-                if(bitmap != null) {
-                    Log.i("bitmapTag", "Bitmap is being retrieved");
-                }
                 bitmapImage = bitmap;
-                if(bitmapImage != null){
-                    Log.i("drawableNull", "Drawable is not null");
-                }else{
-                    Log.i("drawableNotNull", "Drawable is null");
-                }
                 view.setImageBitmap(bitmapImage);
             }
 
