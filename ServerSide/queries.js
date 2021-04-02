7034b687c1f63108040358d3f656587e299ceda7
tjheij <tjheij@gmail.com>
2021-04-02 11:31:37 +0200

    Changed createuser again

--- ServerSide/queries.js
+++ ServerSide/queries.js
@@ -47,27 +47,26 @@ const getUserByUsername = async(request, response) => {
 
 const createUser = (request, response) => {
   
-	console.log(request.body);
-
 	const {username, fullname, email, password, dateofbirth, profilepicture} = request.body;
 
 	pool.query("SELECT * FROM users WHERE username = $1", [username], (error, results) => {
 		if (error) {
 			throw error
 		}
-		
-		if(results.rows[0]) {
-			response.status(404).send("Created")
-			return
+
+		if(results[0]) {
+			response.status(404).end()
 		}
+
+		pool.query("INSERT INTO users (username, fullname, email, password, dateofbirth, profilepicture) VALUES ($1,$2,$3,$4,$5,$6)", [username, fullname, email, password, dateofbirth, profilepicture], (error, results) => {
+			if (error) {
+				throw error
+			}
+			response.status(200).json(results[0])
+		})
 	})
 
-	pool.query("INSERT INTO users (username, fullname, email, password, dateofbirth, profilepicture) VALUES ($1,$2,$3,$4,$5,$6)", [username, fullname, email, password, dateofbirth, profilepicture], (error, results) => {
-		if (error) {
-			throw error
-		}
-		response.status(200).send("Created")
-	})
+	
 }
 
 const updateUser = async(request, response) => {
