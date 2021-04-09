6fe628cd1221bd0cfb7bb9fbc4eb9b5775a3eebd
EzraLeeuwenhage <60776275+EzraLeeuwenhage@users.noreply.github.com>
2021-04-09 15:11:53 +0200

    Update queries.js
    test

--- ServerSide/queries.js
+++ ServerSide/queries.js
@@ -112,7 +112,7 @@ const getRecipeById = (request, response) => {
 const getRecipeByName = (request, response) => {
   const name = request.params.name;
 
-  pool.query("SELECT * FROM recipes WHERE (name ~ $1) OR (name ~ LOWER($1)", [name], (error, results) => {
+  pool.query("SELECT * FROM recipes", [name], (error, results) => {
     if (error) {
       throw error
     }
